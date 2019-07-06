6ebf89a923b112ae253e43550c4483d4f44ed285
Popsana Barida <popsyjunior@gmail.com>
2019-07-06 18:53:04 +0100

    add more comment tests

--- app/Comment.php
+++ app/Comment.php
@@ -20,10 +20,26 @@ class Comment extends Model
     }
 
     public function setMessageAttribute($message) {
-        if (strlen($message) > 500) {
+        if (strlen(trim($message)) < 2) {
+            throw new \ErrorException("Message length must be more than two characters");
+        } else if (strlen($message) > 500) {
             throw new \ErrorException("Message length cannot be greater than 500");
         }
 
         $this->attributes["message"] = $message;
+    }
+
+    function isValidIp($ip_address) {
+        return preg_match("/\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}/", $ip_address);
+    }
+
+    public function setIpAddressAttribute($ip_address) {
+        if (!$this->isValidIp($ip_address)) {
+            throw new \ErrorException("Invalid ip address");
+        } else if (strlen($ip_address) > 500) {
+            throw new \ErrorException("Message length cannot be greater than 500");
+        }
+
+        $this->attributes["ip_address"] = $ip_address;
     }
 }
--- tests/CommentsTest.php
+++ tests/CommentsTest.php
@@ -13,6 +13,21 @@ class CommentsTest extends TestCase
         $this->assertInstanceOf(Comment::class, $comment);
     }
 
+    public function test_comment_must_have_a_valid_message() {
+        $this->expectException(\ErrorException::class);
+        $comment = Comment::addNew("121.12.221.33.8", "");
+    }
+
+    public function test_comment_must_have_a_valid_ip_address() {
+        $this->expectException(\ErrorException::class);
+        $comment = Comment::addNew("123333", "Hello World");
+    }
+
+    public function test_comment_must_have_a_message_and_ip_address() {
+        $this->expectException(\ErrorException::class);
+        $comment = Comment::addNew("", "Hello");
+    }
+
     public function test_comments_cant_be_more_than_500_characters() {
         $this->expectException(\ErrorException::class);
         $really_long_message = "Hello world Endpoint should accept sort parameters to sort by one of name, gender or species in  or descending order Endpoint should also accept a filter parameter to filter by gender or status The response should also return metadata that contains the total number of characters that match the criteria Endpoint should also accept a filter parameter to filter by gender or status The response should also return metadata that contains the total number of characters that match the criteria Endpoint should also accept a filter parameter to filter by gender or status The response should also return metadata that contains the total number of characters that match the criteria";
