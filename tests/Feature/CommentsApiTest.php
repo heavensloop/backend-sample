4dc6ac123ca487edba0da53efbac0dd672ae9208
Popsana Barida <popsyjunior@gmail.com>
2019-07-07 19:01:28 +0100

    break ApiTest into comments and episodes

--- tests/Feature/CommentsApiTest.php
+++ tests/Feature/CommentsApiTest.php
@@ -5,34 +5,9 @@ use Laravel\Lumen\Testing\DatabaseTransactions;
 
 use App\Comment;
 
-class ApiTest extends TestCase
+class CommentsApiTest extends TestCase
 {
     use DatabaseTransactions;
-
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_home_page_returns_version()
-    {
-        $this->get('/');
-
-        $this->assertEquals(
-            env('APP_NAME') . " V" . env('APP_VERSION'), $this->response->getContent()
-        );
-    }
-
-    public function test_episodes_route_returns_episodes()
-    {
-        $this->get('/episodes');
-
-        $content = $this->response->getContent();
-        $data = json_decode($content);
-
-        $this->assertTrue(isset($data->info));
-        $this->assertTrue(isset($data->results));
-    }
 
     public function test_add_new_comment()
     {
---
+++ tests/Feature/EpisodesApiTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+use App\Comment;
+
+class EpisodesApiTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_home_page_returns_version()
+    {
+        $this->get('/');
+
+        $this->assertEquals(
+            env('APP_NAME') . " V" . env('APP_VERSION'), $this->response->getContent()
+        );
+    }
+
+    public function test_episodes_route_returns_episodes()
+    {
+        $this->get('/episodes');
+
+        $content = $this->response->getContent();
+        $data = json_decode($content);
+
+        $this->assertTrue(isset($data->info));
+        $this->assertTrue(isset($data->results));
+    }
+}
